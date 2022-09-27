2b0d954d29857de8c57b68b22fa7b8bc0d51bca1
kawezinLiru <kawe.ribeiro@etec.sp.gov.br>
2022-09-27 14:39:29 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Laravel</title>
+</head>
+
+<body>
+    <center>
+        <h1>Bem vindo ao CRUD Laravel</h1>
+        <h3>Feito por Kawê Cavalcante</h3>
+
+        <br><br>
+
+        <a href="/cadastro">Cadastrar Usuário</a>
+        &nbsp;&nbsp;&nbsp;
+        <a href="/listar">Listar Cadastros</a>
+    </center>
+</body>
+
+</html>
